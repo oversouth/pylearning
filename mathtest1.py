bef2a0fd5c1ac886037bf372ff3fde93b5eaf717
Pavel Jedai <pavel.d.zaikin@gmail.com>
2025-11-25 11:19:05 +0400

    mathtest1 added

---
+++ mathtest1.py
@@ -0,0 +1,6 @@
+num1 = float(input("What is your number 1? "))
+num2 = float(input("What is your number 2? "))
+num3 = float(input("What is your number 3? "))
+
+print("Here is your result:")
+print(num1 + num2+num3)
