5046da403040a195325058e3377aeda767670b1a
Pavel Z <pavel.d.zaikin@gmail.com>
2026-02-23 11:56:20 +0400

    hello from zed

--- Homework/HWtask1L4.py
+++ Homework/HWtask1L4.py
@@ -1,7 +1,7 @@
-try: 
+try:
     num1 = int(input("Enter number 1: "))
     num2 = int(input("Enter number 2: "))
-except ValueError: 
+except ValueError:
     print("Only integers are allowed")
     exit()
 
@@ -11,3 +11,4 @@ if num1 > num2:
     print(num1, " is greater than ", num2)
 elif num1 < num2:
     print(num2, " is greater than ", num1)
+    # hello from zed!
