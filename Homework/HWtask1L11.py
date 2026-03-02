09052c35243149caa6a853ddf8d1923aa9fdb258
Pavel Z <pavel.d.zaikin@gmail.com>
2026-03-02 09:01:51 +0400

    домашка:3

---
+++ Homework/HWtask1L11.py
@@ -0,0 +1,11 @@
+numbers = input().split()
+max = int(numbers[0])
+min = int(numbers[0])
+for n in numbers:
+    n = int(n)
+    if n > max:
+        max = n
+    if n < min:
+        min = n
+print("Максимум:", max)
+print("Минимум:", min)
