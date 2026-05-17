4547d4e31c700a850b18b79c82ad822372b16f90
Pavel Zaikin <pavel.d.zaikin@gmail.com>
2026-05-17 15:44:24 +0400

    bebebe

---
+++ Lessons/intlesson12task1.py
@@ -0,0 +1,30 @@
+class human(animal):
+    def __init__(self, a, b, c)
+        self.name = a
+        self.name = b
+        self.name = c
+    def think(self):
+        pass
+    def code(self):
+        pass
+human1 = human(a:"aboba", b:67695242, c:"black eyes")
+human2 = human(a:"bebebe",b:3, c:"white gray pink eyes")
+human3 = human(a:"kakashka", b:123456789, c:"red eyes")
+class animal:
+    def __init__(self, a, b, c):
+        self.name = a
+        self.name = b
+        self.name = c
+        def breathe(self):
+            pass
+        def sleep(self):
+            pass
+        def eat(self):
+            pass
+        def kaka(self):
+            pass
+        def kill(self):
+            pass
+animal1 = animal(a:"rat", b:"3 years maximum", c:"Rattus norvegicus")
+animal2 = animal(a:"cat", b:"20 years maximum", c:"felis cattus")
+animal3 = animal(a:"racoon", b:"16 years maximum",c:"procyon lotor")
