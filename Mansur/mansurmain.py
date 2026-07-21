bd403a1fe4552a7592e826267c808970c83871d7
Pavel Zaikin <pavel.d.zaikin@gmail.com>
2026-07-21 09:01:54 +0400

    Made auth system and persistent user storage

---
+++ Mansur/mansurmain.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/HelloWorld")
+def helloworld():
+    return "Hello World"
+
+
+@app.post("/new_user")
+def new_user(info: dict):
+    name = info.get("name")
+    pwd = info.get("password")
+    
+    with open(name + ".txt", "w") as file:
+        file.write("name"+" " name\n"password"+ " " pwd)
+
+    return {"message": "user created"}
+
+
+@app.post("/whoami")
+def whoami(info: dict):
+    name = info.get("name")
+
+    userprofile = localdb.get(name)
+
+    if userprofile:
+        return userprofile
+
+    return {"error": "user not found"}
